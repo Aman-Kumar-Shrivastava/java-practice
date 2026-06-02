56619eba88411ffe2172ecf8835baed5c042fcc8
Aman <amanshrivastava185@gmail.com>
2026-06-03 00:26:47 +0530

    add PrimitiveDataInJava class to demonstrate primitive data types

---
+++ basics in java/PrimitiveDataInJava.java
@@ -0,0 +1,35 @@
+public class PrimitiveDataInJava {
+
+    public static void main(String[] args) {
+
+        // Integer Types
+        byte age = 20;
+        short year = 2025;
+        int population = 1000000;
+        long phoneNumber = 9876543210L;
+
+        // Decimal Types
+        float price = 99.99F;
+        double salary = 55000.75;
+
+        // Character Type
+        char grade = 'A';
+
+        // Boolean Type
+        boolean isJavaFun = true;
+
+        System.out.println("=== Primitive Data Types in Java ===");
+
+        System.out.println("byte age = " + age);
+        System.out.println("short year = " + year);
+        System.out.println("int population = " + population);
+        System.out.println("long phoneNumber = " + phoneNumber);
+
+        System.out.println("float price = " + price);
+        System.out.println("double salary = " + salary);
+
+        System.out.println("char grade = " + grade);
+
+        System.out.println("boolean isJavaFun = " + isJavaFun);
+    }
+}
